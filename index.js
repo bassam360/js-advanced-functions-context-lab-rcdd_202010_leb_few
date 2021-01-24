e577b47bf4ec68fe23197337d5ce9c8e2ae68378
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-24 13:32:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
 
 
 
